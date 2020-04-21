44fa52f43816a33c947deaae55dab22992950ab7
github username <annacarey@icloud.com>
2020-04-21 13:52:46 -0600

    got rid of counter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,12 @@ class User < ApplicationRecord
     has_secure_password
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: { minimum: 6 }
-    @@anonymous_counter = 0
 
     def coronavirus_journal_entries
         self.journals.find_by(title: "coronavirus").journal_entries
     end 
 
     def self.generate_anonymous_user(zipcode)
-        @@anonymous_counter += 1
-        User.create(email: "user#{@@anonymous_counter}", zipcode: zipcode, password: "password#{@@anonymous_counter}")
+        User.create(email: "user#{User.last.id + 1}", zipcode: zipcode, password: "anonymous#{User.last.id + 1}")
     end
 end
