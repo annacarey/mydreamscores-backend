d23329b19143983abe99ffa69d06e5ca94572cdb
github username <annacarey@icloud.com>
2020-04-22 15:44:24 -0600

    cors problem

--- config/initializers/google_client_initializer.rb
+++
@@ -1,5 +0,0 @@
-require 'googleauth'
-
-GOOGLE_SERVICE_ACCOUNT_CREDENTIALS=Google::Auth::ServiceAccountCredentials.make_creds(
-    json_key_io: StringIO.new(ENV['GOOGLE_APPLICATION_CREDENTIALS'])
-  )
